65e58e5ccea07614cd0e1ccfe5dc5a3e28384c10
paulclau <paulclaughton2005@gmail.com>
2025-03-15 23:28:42 +1100

    Included svg icons
    lncluded icons
    cleaned up fonts
    finalized spacings between icons

--- frontend/pals-plan-app/src/components/calendar.jsx
+++ frontend/pals-plan-app/src/components/calendar.jsx
@@ -9,25 +9,25 @@ const daysOfWeek = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'];
   return (
     <div className="w-full h-full flex flex-row text-center items-center gap-2">
       <div className="h-full flex flex-col font-[Dongle] font-bold text-4xl">
-        <p className="h-6"></p>
+        <p className="h-12"></p>
         <div className="flex-1">
+          <img src="public/sun-solid.svg" className='w-7 m-4'></img>
           <p>7-12</p>
-          <img src="src/assets/sun-solid.svg"></img>
         </div>
         <div className="flex-1">
+          <img src="public/cloud-solid.svg" className='w-7 m-4'></img>
           <p>12-5</p>
-          <img></img>
         </div>
         <div className="flex-1">
+          <img src="public/moon-solid.svg" className='w-7 m-4'></img>
           <p>5-10</p>
-          <img></img>
         </div>
       </div>
         <div className='h-full flex flex-1 gap-3'>
           {daysOfWeek.map((day, idx) => {
             return (
               <div key={idx} className="h-full flex flex-col flex-1 ">
-                <p className="text-center pb-2 font-[Dongle]">{day}</p>
+                <p className="text-center pb-2 text-4xl font-semibold font-[Dongle]">{day}</p>
                 <Day></Day>
               </div>
             )
--- frontend/pals-plan-app/src/pages/AvailabilityPage.jsx
+++ frontend/pals-plan-app/src/pages/AvailabilityPage.jsx
@@ -7,23 +7,18 @@ const name = "Joe";
 export default function AvailabilityPage() {
 
     return (
-			<div className="h-screen flex flex-col bg-[url('public/AvailabilityPageBackground.png')] bg-cover">
-				<div className='flex flex-col bg-[#FFCF32]  p-8 mx-50 mt-16 rounded-2xl '>
-					<div>
-						<h1 className="text-center text-5xl font-[Slackey]">My Availabilities</h1>
-					</div>
-					<div className='h-96 mt-10 mr-10'>
-						<Calendar></Calendar>
-					</div>
+		<div className="h-screen flex flex-col bg-[url('public/AvailabilityPageBackground.png')] bg-cover">
+			<div className='flex flex-col bg-[#FFCF32]  p-8 mx-50 mt-16 rounded-2xl '>
+				<div>
+					<h1 className="text-center text-5xl font-[Slackey]">My Availabilities</h1>
 				</div>
-				<div className="flex-1 flex items-center justify-center">
-					<button className="btn border-0 bg-[#08BA63] text-white text-lg">Submit</button>
+				<div className='h-96 mt-10 mr-10'>
+					<Calendar></Calendar>
 				</div>
 			</div>
-	
-
-        
-
+			<div className="flex-1 flex items-center justify-center">
+				<button className="btn border-0 bg-[#08BA63] text-white text-lg">Submit</button>
+			</div>
+		</div>
     )
-
 }
