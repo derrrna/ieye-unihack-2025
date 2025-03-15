f81b8d5c203b574cb86c61f6d4664ddab010e2bd
dpab0001 <dannapabayo@gmail.com>
2025-03-15 22:37:10 +1100

    Fixed design of button

--- frontend/pals-plan-app/src/components/LocationBlock.jsx
+++ frontend/pals-plan-app/src/components/LocationBlock.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 export default function LocationBlock() {
     return(
-        <div className="bg-white w-full shadow-sm h-22 m-3 rounded-xl">
+        <button className=" btn btn-outline w-full shadow-sm h-22 m-3 rounded-xl ">
             <div className="flex w-full h-full">
-                <img src="public/locationIcon.svg" className="w-10 m-5"/>
+                <img src="public/locationIcon.svg" className="w-10 m-5 color-white"/>
                 <div className="flex flex-col">
-                    <p className="text-4xl pt-4 leading-none">O'Brien IceHouse</p>
-                    <p className="text-2xl"> Ground/105 Pearl River Rd, Docklands VIC 3008 </p>
+                    <p className="text-4xl pt-4 leading-none text-start">O'Brien IceHouse</p>
+                    <p className="text-3xl font-light">Ground/105 Pearl River Rd, Docklands VIC 3008 </p>
                 </div>
             </div>
-        </div>
+        </button>
     )
 }
