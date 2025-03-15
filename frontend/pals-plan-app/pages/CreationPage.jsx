a8c2aa539af92f391b268e694ca403f61baabc15
dpab0001 <dannapabayo@gmail.com>
2025-03-15 15:07:49 +1100

    Positioned headers for landing page

--- frontend/pals-plan-app/pages/CreationPage.jsx
+++ frontend/pals-plan-app/pages/CreationPage.jsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import '../styles/CreationPage.css';
 
 export default function CreationPage() {
 
     return (
-        <div>
+        <div className="w-screen h-screen flex flex-row bg-gray-100">
 
-            {/* Content */}
-            <div className="w-screen h-screen flex flex-row bg-gray-100">
-                
-                {/* Logo + Descriptive Text*/}
-                <div className="w-1/2">
-                    <h1 className="text-xl">Pals Plan</h1>
-                    <h2 className="text-base">Plan a hangout in less than 5 minutes!</h2>
+            {/* Logo + Descriptive Text*/}
+            <div className="lg:w-1/2 flex flex-col">
+                <div className="lg:pl-50 ">
+                    <h1 className="lg:pt-40 lg:text-5xl">Pals Plan</h1>
+                    <h2 className="lg:mt-5 lg:text-xl">Plan a hangout in less than 5 minutes!</h2>
                 </div>
+            </div>
 
-                {/* Form */}
-                <div className="w-1/2">
-                    <h1> hi</h1>
-                </div>
+            {/* Form */}
+            <div className="lg:w-1/2">
+                <h1>hi</h1>
             </div>
 
         </div>
