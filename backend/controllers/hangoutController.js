97169d955344af52f7f62f2fa5ba4e79ecbd9e9d
nmar0047 <nmar0047@student.monash.edu>
2025-03-15 17:39:33 +1100

    added documentation to hangoutController

--- backend/controllers/hangoutController.js
+++ backend/controllers/hangoutController.js
@@ -1,6 +1,16 @@
 const Hangout = require('../models/Hangout');
 
 
+/**
+ * Asynchronously retrieves a list of all hangouts from the database.
+ *
+ * This function handles HTTP requests and sends a response to the client.
+ * If the retrieval is successful, it sends a status of 200 with the hangouts data in JSON format.
+ * In case of an error during the retrieval process, it responds with a status of 500 and an error message.
+ *
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP response object.
+ */
 const getHangouts = async (req, res) => {
     try {
         const hangouts = await Hangout.find();
@@ -10,15 +20,25 @@ const getHangouts = async (req, res) => {
     }
 };
 
+/**
+ * Asynchronous function to create a new hangout and save it in the database.
+ *
+ * @param {Object} req - Express request object containing data necessary to create a hangout.
+ * @param {Object} req.body - The body of the request containing hangout details.
+ * @param {string} req.body.name - The name of the hangout.
+ * @param {string} req.body.startDate - The start date of the hangout.
+ * @param {string} req.body.location - The location of the hangout.
+ * @param {string} req.body.link - Optional link for the hangout.
+ * @param {Array<string>} req.body.attendees - List of attendees for the hangout.
+ * @param {Object} res - Express response object used to send a response back to the client.
+ *
+ * @returns {Promise<void>} Sends a response with the created hangout details if successful (HTTP 201),
+ * or an error message if the operation fails (HTTP 500).
+ */
 const createHangouts = async (req, res) => {
 
     try {
         const {name, startDate, location, link, attendees} = req.body;
-
-        // Validate required inputs
-        // if (!name || !startDate || !location || !link || !attendees) {
-        //     return res.status(400).json({message: 'Please provide name, start date, and location for the hangout.'});
-        // }
 
         const newHangout = new Hangout({name, startDate, location, link, attendees});
 
