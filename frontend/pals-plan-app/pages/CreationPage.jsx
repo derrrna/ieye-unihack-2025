3fd492a85b91fcfe1fa6efbb680947ad7968440d
dpab0001 <dannapabayo@gmail.com>
2025-03-15 14:51:10 +1100

    Added headers

--- frontend/pals-plan-app/pages/CreationPage.jsx
+++ frontend/pals-plan-app/pages/CreationPage.jsx
@@ -5,13 +5,14 @@ export default function CreationPage() {
 
     return (
         <div>
-            
+
             {/* Content */}
             <div className="w-screen h-screen flex flex-row bg-gray-100">
                 
                 {/* Logo + Descriptive Text*/}
                 <div className="w-1/2">
-                    <h1>hi</h1>
+                    <h1 className="text-xl">Pals Plan</h1>
+                    <h2 className="text-base">Plan a hangout in less than 5 minutes!</h2>
                 </div>
 
                 {/* Form */}
