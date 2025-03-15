5c1995e9a96f352faa8f27826999b60e5c9cbeda
dpab0001 <dannapabayo@gmail.com>
2025-03-15 22:24:51 +1100

    Added static location blocks

--- frontend/pals-plan-app/src/components/LocationBlock.jsx
+++ frontend/pals-plan-app/src/components/LocationBlock.jsx
@@ -2,9 +2,13 @@ import React from "react";
 
 export default function LocationBlock() {
     return(
-        <div className="bg-white w-full shadow-sm h-20 m-3 rounded-xl">
-            <div className="flex">
-                <h1>O'Brien IceHouse</h1>
+        <div className="bg-white w-full shadow-sm h-22 m-3 rounded-xl">
+            <div className="flex w-full h-full">
+                <img src="public/locationIcon.svg" className="w-10 m-5"/>
+                <div className="flex flex-col">
+                    <p className="text-4xl pt-4 leading-none">O'Brien IceHouse</p>
+                    <p className="text-2xl"> Ground/105 Pearl River Rd, Docklands VIC 3008 </p>
+                </div>
             </div>
         </div>
     )
--- frontend/pals-plan-app/src/pages/SuggestionPage.jsx
+++ frontend/pals-plan-app/src/pages/SuggestionPage.jsx
@@ -39,7 +39,7 @@ export default function SuggestionPage() {
                     <LocationBlock/>
                     <LocationBlock/>
                     <LocationBlock/>
-                    <LocationBlock/>
+
                 </div>
             </div>
 
