a98db69da5fbeba0bd0fe33cc01f78b2f9caeb07
dpab0001 <dannapabayo@gmail.com>
2025-03-16 01:09:41 +1100

    Made locations linked to an array

--- frontend/pals-plan-app/src/components/LocationBlock.jsx
+++ frontend/pals-plan-app/src/components/LocationBlock.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-export default function LocationBlock() {
+export default function LocationBlock(props) {
     return(
         <button className=" btn btn-outline w-full shadow-sm h-22 m-3 rounded-xl ">
             <div className="flex w-full h-full">
                 <img src="public/locationIcon.svg" className="w-10 m-5 color-white"/>
                 <div className="flex flex-col">
-                    <p className="text-4xl pt-4 leading-none text-start">O'Brien IceHouse</p>
-                    <p className="text-3xl font-light">Ground/105 Pearl River Rd, Docklands VIC 3008 </p>
+                    <p className="text-4xl pt-4 leading-none text-start">{props.locationName}</p>
+                    <p className="text-3xl font-light">{props.address}</p>
                 </div>
             </div>
         </button>
--- frontend/pals-plan-app/src/pages/SuggestionPage.jsx
+++ frontend/pals-plan-app/src/pages/SuggestionPage.jsx
@@ -3,6 +3,17 @@ import LocationBlock from "../components/LocationBlock";
 
 export default function SuggestionPage() {
 
+    /*NOTE: this array should contain the top 5 locations suggested by the AI. For now this is hardcoded.*/
+    /* Refer to line 48 to see where these objects are used*/
+    const topLocations = [
+        {locationName: "O'Brien Icehouse", address: "Ground/105 Pearl River Rd, Docklands VIC 3008"},
+        {locationName: "Melbourne Ice Skating Centre", address: "98A/1-5 The Blvd, Altona North VIC 3025"},
+        {locationName: "Winter Village", address: "Federation Square, Melbourne VIC 3000"},
+        {locationName: "The Ice Arena", address: "6-14 Kelletts Rd, Rowville VIC 3178"},
+        {locationName: "The Ice Arena", address: "6-14 Kelletts Rd, Rowville VIC 3178"}
+    ];
+
+
     return (
         
         <div className="w-screen h-screen flex bg-[url('public/SuggestionPageBackground.png')] bg-cover">
@@ -15,7 +26,7 @@ export default function SuggestionPage() {
 
                 <div className="pl-50 mt-10 font-[Dongle]">
 
-                    {/* Know what to do */}
+                    {/* user know what to do */}
                     <h1 className="text-6xl font-bold"> Activity Generator</h1>
                     <h3 className="text-4xl mt-5"> What plan you feeling? </h3>
                     <div className="mt-3">
@@ -23,7 +34,7 @@ export default function SuggestionPage() {
                         <button className="btn ml-3 text-xl text-white bg-[#5E93E8] border-none rounded-xl"> Suggest </button>
                     </div>
                     
-                    {/* Don't know what to do*/}
+                    {/* user don't know what to do*/}
                     <h3 className="text-4xl mt-15"> No idea? </h3>
                     <button className="btn mt-2 w-1/4 text-xl text-white bg-[#F8574F] border-none rounded-xl"> Suggest for me! </button>
 
@@ -31,16 +42,17 @@ export default function SuggestionPage() {
 
             </div>
 
-            {/* location side*/}
+            {/* location suggestions side*/}
             <div className="w-1/2 h-full text-black font-[Dongle]">
-                <h1 className="text-6xl font-semibold pt-20">We Suggest...</h1>
-                <div className="bg-grey w-6/7 h-3/4 flex flex-col">
-                    <LocationBlock/>
-                    <LocationBlock/>
-                    <LocationBlock/>
-                    <LocationBlock/>
+                <h1 className="text-6xl font-semibold pt-10">We Suggest...</h1>
 
+                {/*NOTE: For each object in the array, it creates a LocationBlock component and fills in that location's unique details.*/}
+                <div className="bg-grey w-6/7 h-3/4 flex flex-col">
+                    {topLocations.map( (location) =>
+                        <LocationBlock locationName ={location.locationName} address={location.address}/>
+                    )}
                 </div>
+
             </div>
 
         </div>
