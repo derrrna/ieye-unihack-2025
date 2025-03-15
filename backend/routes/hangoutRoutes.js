d71be902d7ce5a91eb782b722445191cc0b48354
kayden-dev <61534240+kayden-dev@users.noreply.github.com>
2025-03-15 17:33:40 +1100

    create get hangout dates route

--- backend/routes/hangoutRoutes.js
+++ backend/routes/hangoutRoutes.js
@@ -3,5 +3,6 @@ const hangoutController = require("../controllers/hangoutController.js");
 const router = express.Router()
 
 router.post("/", hangoutController.createHangout);
+router.get("/dates", hangoutController.dates);
 
 module.exports = router;
