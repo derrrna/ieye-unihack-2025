0716cea66cc86905aa07304c89b0581321cde596
Kayden Nguyen <61534240+kayden-dev@users.noreply.github.com>
2025-03-16 13:56:42 +1100

    code functioning, but divs out of order

--- frontend/pals-plan-app/src/pages/SuggestionPage.jsx
+++ frontend/pals-plan-app/src/pages/SuggestionPage.jsx
@@ -10,7 +10,6 @@ export default function SuggestionPage() {
 	const [suggestedActivities, setSuggestedActivities] = useState([]); // State to store the array of suggestions
 	const [currentActivityIndex, setCurrentActivityIndex] = useState(0); // State to track the current index
 	const [currentInputValue, setCurrentInputValue] = useState(""); // State to track the input value
-    const { hangoutId } = useParams();
     /*NOTE: this array should contain the top 5 locations suggested by the AI. For now this is hardcoded.*/
     /* Refer to line 48 to see where these objects are used*/
     const [topLocations, setTopLocations] = useState([]);
@@ -83,15 +82,7 @@ export default function SuggestionPage() {
 			if (!response.ok) {
 				throw new Error("Failed to fetch suggestions");
 			}
-                    {/* user know what to do */}
-                    <h1 className="text-6xl font-bold"> Activity Generator</h1>
-                    <h3 className="text-4xl mt-5"> What plan you feeling? </h3>
-                    <div className="mt-3">
-                    <form onSubmit={handleFormSubmit}>
-                        <input type="text" name="name" placeholder="Enter an activity..." className="bg-[#EFEFEF] input rounded-xl text-xl" autocomplete="off" />
-                        <button type="submit" className="btn ml-3 text-xl text-white bg-[#5E93E8] border-none rounded-xl"> Suggest </button>
-                    </form>
-
+            {/* user know what to do */}
 			const data = await response.json();
 			console.log("Suggestions received:", data);
 
@@ -116,87 +107,56 @@ export default function SuggestionPage() {
 
 				<div className="pl-50 mt-10 font-[Dongle]">
 					{/* user know what to do */}
-					<h1 className="text-6xl font-bold"> Activity Generator</h1>
-					<h3 className="text-4xl mt-5"> What plan you feeling? </h3>
-					<div className="mt-3">
-						<form
-							action={`http://localhost:3000/event/${hangoutId}/locations`}
-						>
-							<input
-								type="text"
-								name="name"
-								placeholder="Enter an activity..."
-								value={currentInputValue} // Bind the input value to the state
-								onChange={
-									(e) => setCurrentInputValue(e.target.value) // Update the input value on user input
-								}
-								className="bg-[#EFEFEF] input rounded-xl text-xl"
-							/>
-							<button
-								type="submit"
-								className="btn ml-3 text-xl text-white bg-[#5E93E8] border-none rounded-xl"
-							>
-								Suggest
-							</button>
-						</form>
-					</div>
+                    <h1 className="text-6xl font-bold"> Activity Generator</h1>
+                    <h3 className="text-4xl mt-5"> What plan you feeling? </h3>
+                    <div className="mt-3">
+                        <form onSubmit={handleFormSubmit}>
+                            <input type="text" name="name" value={suggestedActivities[currentActivityIndex]} className="bg-[#EFEFEF] input rounded-xl text-xl" autoComplete="off"/>
+                            <button type="submit" className="btn ml-3 text-xl text-white bg-[#5E93E8] border-none rounded-xl"> Suggest </button>
+                        </form>
+                    </div>
 
 					{/* user don't know what to do */}
 					<div className="flex flex-col gap-3 w-1/2">
-						<h3 className="text-4xl mt-5"> No idea? </h3>
-            {/* location suggestions side*/}
-            <div className="w-1/2 h-full text-black font-[Dongle]">
-                <h1 className="text-6xl font-semibold pt-10">We Suggest In The Area...</h1>
-
-                {/*NOTE: For each object in the array, it creates a LocationBlock component and fills in that location's unique details.*/}
-                <div className="bg-grey w-6/7 h-3/4 flex flex-col">
-                    {topLocations.map( (location) =>
-                        <LocationBlock locationName ={location.name} address={location.address} suggestingUser={userId} hangoutId={hangoutId}/>
-                    )}
+					<h3 className="text-4xl mt-5"> No idea? </h3>
+                        <select
+                                value={selectedCategory}
+                                onChange={(e) =>
+                                    setSelectedCategory(e.target.value)
+                                }
+                                className={`select text-2xl ${
+                                    isError ? "select-error" : ""
+                                }`} // Add error class if isError is true
+                            >
+                                <option value="" disabled>
+                                    Pick a category
+                                </option>
+                                <option value="sports">Sports</option>
+                                <option value="chill">Chill</option>
+                                <option value="creative">Creativity</option>
+                                <option value="entertainment">Entertainment</option>
+                                <option value="food">Food</option>
+                                <option value="study">Study</option>
+                        </select>
+                            
+                        <button
+                            onClick={onSuggestForMeClick}
+                            className="btn mt-2  text-xl text-white bg-[#F8574F] border-none rounded-xl"
+                        >
+                            Suggest for me!
+                        </button>
+                    </div>
+                {/* location suggestions side*/}
+                <div className="w-1/2 h-full text-black font-[Dongle]">
+                    <h1 className="text-6xl font-semibold pt-10">We Suggest In The Area...</h1>
+                    {/*NOTE: For each object in the array, it creates a LocationBlock component and fills in that location's unique details.*/}
+                    <div className="bg-grey w-6/7 h-3/4 flex flex-col">
+                        {topLocations.map( (location) =>
+                            <LocationBlock locationName ={location.name} address={location.address} suggestingUser={userId} hangoutId={hangoutId}/>
+                        )}
+                    </div>
                 </div>
-
-						<select
-							value={selectedCategory}
-							onChange={(e) =>
-								setSelectedCategory(e.target.value)
-							}
-							className={`select text-2xl ${
-								isError ? "select-error" : ""
-							}`} // Add error class if isError is true
-						>
-							<option value="" disabled>
-								Pick a category
-							</option>
-							<option value="sports">Sports</option>
-							<option value="chill">Chill</option>
-							<option value="creative">Creativity</option>
-							<option value="entertainment">Entertainment</option>
-							<option value="food">Food</option>
-							<option value="study">Study</option>
-						</select>
-						<button
-							onClick={onSuggestForMeClick}
-							className="btn mt-2  text-xl text-white bg-[#F8574F] border-none rounded-xl"
-						>
-							Suggest for me!
-						</button>
-					</div>
-				</div>
-			</div>
-
-			{/* location suggestions side */}
-			<div className="w-1/2 h-full text-black font-[Dongle]">
-				<h1 className="text-6xl font-semibold pt-10">We Suggest...</h1>
-
-				{/* NOTE: For each object in the array, it creates a LocationBlock component and fills in that location's unique details. */}
-				<div className="bg-grey w-6/7 h-3/4 flex flex-col">
-					{topLocations.map((location) => (
-						<LocationBlock
-							locationName={location.locationName}
-							address={location.address}
-						/>
-					))}
-				</div>
+			</div>	
 			</div>
 		</div>
 	);
