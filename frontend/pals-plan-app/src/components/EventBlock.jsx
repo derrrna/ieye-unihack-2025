285bc64ca94d2125ae033a65f2d2d42327495960
Kayden Nguyen <61534240+kayden-dev@users.noreply.github.com>
2025-03-16 15:21:31 +1100

    added temporary like system

--- frontend/pals-plan-app/src/components/EventBlock.jsx
+++ frontend/pals-plan-app/src/components/EventBlock.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function EventBlock(props) {
+    const [likes, setLikes] = useState(0);
 
     {/* This function makes it so that when the event button is clicked, it leads the user to the hyperlink of the address*/}
     function handleEventClick() {
@@ -9,6 +10,7 @@ export default function EventBlock(props) {
 
     function handleLikeClick(event) {
         event.stopPropagation(); 
+        setLikes(likes + 1);
         {/*TODO: Add code here to update like count of current activity*/}
     }
 
@@ -19,7 +21,7 @@ export default function EventBlock(props) {
 
             <button class="btn rounded-xl border-none shadow-none hover:bg-[#F8574F]" onClick={handleLikeClick}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" class="size-[1.2em]"><path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" /></svg>
-                {props.totalLikes}
+                {likes}
             </button>
 
         </button>
